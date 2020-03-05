89ed50fe6718d9f1d2b613eaf0b01bfd53cee936
alan-serrano <alan.serrano.pch@gmail.com>
2020-03-05 10:45:52 -0400

    Removing the state on the App - The state should live just in Grid

--- src/App.js
+++ src/App.js
@@ -3,11 +3,9 @@ import './App.scss'
 import Grid from './Components/Grid';
 
 function App() {
-    // coordSnake is an object that has the coordinates of each part of the snake
-    const [coordSnake, setCoordSnake] = useState({'r0c0': null}); 
     return (
         <div className="app">
-            <Grid size="32" coordSnake={coordSnake}/>
+            <Grid size="32"/>
         </div>
     );
 }
