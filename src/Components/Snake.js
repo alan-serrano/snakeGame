a5aee6f31ca4e58d72c3522d1ae85bb492c24d2c
alan-serrano <alan.serrano.pch@gmail.com>
2020-03-05 12:27:46 -0400

    If "S" is greater than 1,shift values every update

--- src/Components/Snake.js
+++ src/Components/Snake.js
@@ -4,10 +4,26 @@ export default class Snake {
         this.y = 0;
         this.xSpeed = 1;
         this.ySpeed = 0;
+        this.tail = [{x:0, y:0}]
     }
 
     update() {
         this.x += this.xSpeed;
         this.y += this.ySpeed;
+
+        // Shifting the values every update
+        for( let i = 0; i < this.tail.length; i++ ) {
+            if(this.tail[i+1]) {
+                this.tail[i] = this.tail[i + 1];
+            }
+        }
+
+        // Adding new value to the head of the snake
+        this.tail[this.tail.length - 1] = {x: this.x, y: this.y};
+    }
+
+    grow() {
+        // Add the last address
+        this.tail.unshift(this.tail[0]);
     }
 }
