650762a01d3ae939579dc5aaafbb6f73b0355587
alan-serrano <alan.serrano.pch@gmail.com>
2020-03-13 19:59:07 -0400

    Updates

---
+++ precache-manifest.57f6efff946f29d1287988802cf676f6.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "454afd85d292f7cc27f791a4b1972508",
+    "url": "/snakeGame/index.html"
+  },
+  {
+    "revision": "8ee585d88eb9eaf71061",
+    "url": "/snakeGame/static/css/main.4ddff77e.chunk.css"
+  },
+  {
+    "revision": "e281a544a8676b7ad8bc",
+    "url": "/snakeGame/static/js/2.6e7e18d2.chunk.js"
+  },
+  {
+    "revision": "5356fa2f66e46e6c05e4cbe319ac7f1d",
+    "url": "/snakeGame/static/js/2.6e7e18d2.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8ee585d88eb9eaf71061",
+    "url": "/snakeGame/static/js/main.b58f6c63.chunk.js"
+  },
+  {
+    "revision": "bf9fbd3154e08694fe7a",
+    "url": "/snakeGame/static/js/runtime-main.957338d3.js"
+  }
+]);
